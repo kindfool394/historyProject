c4c7d78aa4ad9585708f3ea1e655e2d319f2aa15
Kulakova Alina <alalkulakova@edu.hse.ru>
2026-05-21 10:21:50 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/historyProject1/',
+  base: '/historyProject/',
   plugins: [react(), tailwindcss()],
 })
